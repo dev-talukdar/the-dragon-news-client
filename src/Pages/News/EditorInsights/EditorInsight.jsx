c711ad8b49b233aa5e7315a8cd6e8b5c2a1ab377
dev-talukdar <khejurbd@gmail.com>
2023-05-02 17:58:50 +0600

    editor inisght added

---
+++ src/Pages/News/EditorInsights/EditorInsight.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+import editorThumb from '../../../assets/editorsInsight1.png' 
+import editorThumb1 from '../../../assets/editorsInsight2.png' 
+import editorThumb2 from '../../../assets/editorsInsight3.png' 
+
+const EditorInsight = () => {
+    return (
+        <CardGroup className='gap-3'>
+      <Card>
+        <Card.Img variant="top" src={editorThumb} />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={editorThumb1} />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={editorThumb2} />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+    );
+};
+
+export default EditorInsight;
--- src/Pages/News/News/NewsDetail.jsx
+++ src/Pages/News/News/NewsDetail.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom'; 
+import EditorInsight from '../EditorInsights/EditorInsight';
 
 const NewsDetail = () => {
     const news = useLoaderData();
     const { _id, title, details, image_url, category_id, thumbnail_url, author, total_view, rating } = news;
     return (
+      <div className='mb-5'>
         <Card>
       <Card.Img variant="top" src= {image_url} className='img-fluid' />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text> {details}</Card.Text>
-        <Link to={`/category/${category_id}`}><Button variant="danger"><FaArrowLeft></FaArrowLeft> All News in this Category</Button></Link>
-        
+        <Link to={`/category/${category_id}`}><Button variant="danger"><FaArrowLeft></FaArrowLeft> All News in this Category</Button></Link> 
       </Card.Body>
     </Card>
+    <div className='mt-5'>
+      <EditorInsight></EditorInsight>
+    </div>
+
+    </div>
     );
 };
 
